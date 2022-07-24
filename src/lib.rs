875661aa6a9dd5257364322641ac306fa5062106
NaokiM03 <naokim03.lib+dev@gmail.com>
2022-07-24 16:14:55 +0900

    first implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod _diff_ba_impl {
+    trait Paint {
+        fn red(&self) -> String;
+        fn green(&self) -> String;
+    }
+    impl Paint for str {
+        fn red(&self) -> String {
+            format!("\u{1b}[31m{}\u{1b}[0m", self)
+        }
+        fn green(&self) -> String {
+            format!("\u{1b}[32m{}\u{1b}[0m", self)
+        }
+    }
+
+    pub fn _dbg(before: &str, after: &str) {
+        for diff in diff::lines(before, after) {
+            match diff {
+                diff::Result::Left(l) => println!("{}", format!("- {l}").red()),
+                diff::Result::Both(l, _) => println!("{}", format!("  {l}")),
+                diff::Result::Right(r) => println!("{}", format!("+ {r}").green()),
+            }
+        }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub mod diff_ba {
+    #[macro_export]
+    macro_rules! dbg_ {
+        ($val:expr, $b:block) => {{
+            let before = format!("{:#?}", $val);
+            let value = $b;
+            let after = format!("{:#?}", $val);
+            _diff_ba_impl::_dbg(&before, &after);
+            value
+        }};
     }
+    pub use dbg_ as dbg;
+}
+pub mod prelude {
+    pub use crate::{_diff_ba_impl, diff_ba};
 }
